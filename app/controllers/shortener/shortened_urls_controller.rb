1b04782562753811056a1cc9e0ebe585ac9d2d57
Isaac Norman <isaacdnorman@gmail.com>
2019-05-17 14:29:34 +1000

    Convert parameters to hash
    This allows us to continue to use `.except!`

--- app/controllers/shortener/shortened_urls_controller.rb
+++ app/controllers/shortener/shortened_urls_controller.rb
@@ -18,7 +18,7 @@ class Shortener::ShortenedUrlsController < ActionController::Base
         ActiveRecord::Base.connection.close
       end
 
-      params.except! *[:id, :action, :controller]
+      params.to_h.except! *[:id, :action, :controller]
       url = sl.url
 
       if params.present?
